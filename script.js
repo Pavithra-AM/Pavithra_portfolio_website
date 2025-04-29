e05f9e06b94c7759a01523557b2c2010e7e0237c
Pavithra-AM <pavithraampavi@gmail.com>
2025-04-29 17:50:52 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Highlight the active section in navbar
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 70;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute('id');
+    }
+  });
+  
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
+});
